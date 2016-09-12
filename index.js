523c136a651b50d3904e01b02639a072b43ed637
Alexandro <arvitaly@list.ru>
2016-09-12 10:32:55 +0700

    change esprima by acorn

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-var esprima = require('esprima');
+
 var walk = require('esprima-walk');
+var acorn = require('acorn');
 var escodegen = require('escodegen');
 module.exports = function (source, callback) {
-    var ast = esprima.parse(source);
+    var ast = acorn.parse(source);
     walk(ast, function (node) {
         if (node.type == "CallExpression") {
             if (node.callee.type == "Identifier" && node.callee.name == "require") {
