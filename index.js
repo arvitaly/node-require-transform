7cfa3affec9e34ab86e77273aad0243ba8b9aacb
Alexandro <arvitaly@list.ru>
2016-09-12 10:37:28 +0700

    change walker

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
-
-var walk = require('esprima-walk');
+var walk = require('estree-walker').walk;
 var acorn = require('acorn');
 var escodegen = require('escodegen');
 module.exports = function (source, callback) {
     var ast = acorn.parse(source);
-    walk(ast, function (node) {
-        if (node.type == "CallExpression") {
-            if (node.callee.type == "Identifier" && node.callee.name == "require") {
-                var res = callback(node.arguments.map((l) => {
-                    return l.value;
-                }));
-                for (var i = 0; i < res.length; i++) {
-                    node.arguments[i] = { type: "Literal", value: res[i], raw: "'" + res[i] + "'" };
+    walk(ast, {
+        enter: function (node) {
+            if (node.type == "CallExpression") {
+                if (node.callee.type == "Identifier" && node.callee.name == "require") {
+                    var res = callback(node.arguments.map((l) => {
+                        return l.value;
+                    }));
+                    for (var i = 0; i < res.length; i++) {
+                        node.arguments[i] = { type: "Literal", value: res[i], raw: "'" + res[i] + "'" };
+                    }
                 }
             }
         }
